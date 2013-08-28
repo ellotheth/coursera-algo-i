43c299bbd68730a376a9f179300838bd592a5c6b
ellotheth <ellotheth@users.noreply.github.com>
2013-08-27 18:44:07 -0600

    might as well do something useful with TestPercolationStats

--- percolation/TestPercolationStats.java
+++ percolation/TestPercolationStats.java
@@ -4,6 +4,15 @@ import org.junit.Before;
 import org.junit.After;
 
 public class TestPercolationStats {
+    private PercolationStats s;
+    @Before
+    public void setUp() {
+        s = new PercolationStats(100, 50);
+    }
+
     @Test
-    public void test_none() {}
+    public void test_mean() {
+        double t = s.mean();
+        assertTrue(t > 0.58 && t < 0.61);
+    }
 }

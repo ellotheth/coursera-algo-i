743aa1bcc8230d7211ef9dda1e4e04c89bd9bec0
ellotheth <ellotheth@users.noreply.github.com>
2013-09-03 02:22:40 -0600

    week 2 assign: empty Subset test

---
+++ randomizedqueue/TestSubset.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+public class TestSubset {
+
+    @Before
+    public void set_up() {}
+
+    @After
+    public void tear_down() {}
+
+    @Test
+    public void test() { /* nothing to test here yet */ }
+
+}
+

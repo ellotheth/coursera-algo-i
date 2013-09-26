c8dc809f88ab9c7851f888b20a766af0c9a42093
ellotheth <ellotheth@users.noreply.github.com>
2013-09-25 22:48:42 -0600

    week 4 assign: empty tests for Solver because it's a headache

---
+++ 8puzzle/TestSolver.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+public class TestSolver {
+
+    @Test
+    public void test() {}
+
+}
+

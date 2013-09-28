a6cd9259f03c1b003099c1a726aef0c31d93cfc9
ellotheth <ellotheth@users.noreply.github.com>
2013-09-28 16:58:34 -0600

    tests for KdTree.range()

--- kdtrees/TestKdTree.java
+++ kdtrees/TestKdTree.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
@@ -77,6 +80,74 @@ public class TestKdTree {
 
         assertFalse(k.contains(new Point2D(0.4, 0.4)));
         assertFalse(k.contains(new Point2D(0.0, 0.0)));
+    }
+
+    @Test
+    public void test_empty_range() {
+        Iterator<Point2D> iter = k.range(new RectHV(0, 0, 1, 1)).iterator();
+        assertFalse(iter.hasNext());
+    }
+
+    @Test
+    public void test_single_range() {
+        Point2D p = new Point2D(1, 1);
+        k.insert(p);
+
+        Iterator<Point2D> iter = k.range(new RectHV(0, 0, 1, 1)).iterator();
+        assertTrue(iter.hasNext());
+        assertTrue(p.equals(iter.next()));
+        assertFalse(iter.hasNext());
+    }
+
+    @Test
+    public void test_multiple_range() {
+        Point2D[] points = get_points();
+        for (Point2D point : points) k.insert(point);
+
+        Iterator<Point2D> iter = k.range(new RectHV(0, 0, 1, 1)).iterator();
+
+        int cnt = 0;
+        while (iter.hasNext()) {
+            cnt++;
+            Point2D p = iter.next();
+            Arrays.asList(points).contains(p);
+        }
+        assertFalse(iter.hasNext());
+        assertEquals(points.length, cnt);
+    }
+
+    @Test
+    public void test_selected_range() {
+        Point2D[] points = new Point2D[]{
+            new Point2D(.5, .5),
+            new Point2D(.7, .8),
+            new Point2D(.1, .1),
+            new Point2D(1, 0),
+            new Point2D(.3, .9),
+        };
+        for (Point2D point : points) k.insert(point);
+        assertEquals(5, k.size());
+
+        Iterator<Point2D> iter = k.range(new RectHV(.6, .6, .8, .8)).iterator();
+        assertTrue(iter.hasNext());
+        iter.next().equals(points[1]);
+        assertFalse(iter.hasNext());
+
+        iter = k.range(new RectHV(0, 0, .6, .6)).iterator();
+        assertTrue(iter.hasNext());
+        iter.next().equals(points[2]);
+        assertTrue(iter.hasNext());
+        iter.next().equals(points[0]);
+        assertFalse(iter.hasNext());
+    }
+
+    @Test
+    public void test_not_empty_no_range() {
+        Point2D[] points = get_points();
+        for (Point2D point : points) k.insert(point);
+
+        Iterator<Point2D> iter = k.range(new RectHV(.4, .4, 1, 1)).iterator();
+        assertFalse(iter.hasNext());
     }
 }
 
